187984fda9e151cb9729f94160f15bcbdfdc1cb2
Akmenos <chrischavesj@gmail.com>
2019-06-30 10:57:23 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Bundle params = new Bundle();
+                 params.putString("mood", "alert");
+                 params.putString("userID", "Cory&Chris");
+                 mFirebaseAnalytics.logEvent("test_event", params);
+             }
+        });
+    }
+}
