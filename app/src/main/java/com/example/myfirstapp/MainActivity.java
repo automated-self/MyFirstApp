070a5a22ce5447ff8a149e0695e7b58fc31f0c6b
Cory Perkins <link2cory@gmail.com>
2019-07-28 12:10:52 -0700

    WIP

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -14,13 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        presenter = new Presenter();
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-                 Bundle params = new Bundle();
-                 params.putString("mood", "alert");
-                 params.putString("userID", "Cory&Chris");
-                 mFirebaseAnalytics.logEvent("test_event", params);
+                 presenter.logActivity(findViewById(R.id.activity_name));
+
+
+
+//                 Bundle params = new Bundle();
+//                 params.putString("mood", "alert");
+//                 params.putString("userID", "Cory&Chris");
+//                 mFirebaseAnalytics.logEvent("test_event", params);
              }
         });
     }
